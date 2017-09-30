51ec8056c92147aaa1f5d4eecfbd802004a31b7a
Friedrich von Never <friedrich@fornever.me>
2017-09-30 20:25:25 +0700

    #1: retrieve variable values from the registry

--- Export-Env.ps1
+++ Export-Env.ps1
@@ -3,14 +3,44 @@ param (
     [string] $OutPath
 )
 
-[System.EnvironmentVariableTarget[]] $scopes = @(
-    'Machine'
-    'User'
+$scopes = @(
+    [EnvironmentVariableTarget]::Machine
+    [EnvironmentVariableTarget]::User
 )
+
+function Get-EnvironmentVariables($scope) {
+    $registryKey =
+        if ($scope -eq [EnvironmentVariableTarget]::Machine) {
+            [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey(
+                'SYSTEM\CurrentControlSet\Control\Session Manager\Environment')
+        } elseif ($scope -eq [EnvironmentVariableTarget]::User) {
+            [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment')
+        } else {
+            throw "Unknown registry scope: $scope"
+        }
+
+    try {
+        $variables = $registryKey.GetValueNames()
+        $scopeResult = @{}
+        foreach ($name in $variables) {
+            $value = $registryKey.GetValue(
+                $name,
+                $null,
+                [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+            if ($value -ne $null) {
+                $scopeResult[$name] = $value
+            }
+        }
+
+        $scopeResult
+    } finally {
+        $registryKey.Dispose()
+    }
+}
 
 $result = @{}
 $scopes | ForEach-Object {
-    $result[$_.ToString()] = [Environment]::GetEnvironmentVariables($_)
+    $result[$_.ToString()] = Get-EnvironmentVariables $_
 }
 
 $result | ConvertTo-Json | Out-File $OutPath -Encoding utf8
